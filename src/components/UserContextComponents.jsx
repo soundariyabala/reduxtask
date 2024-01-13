992729c9be0276e36cf56cc9ed819b144687b330
soundariyabala <balasoundariya.ravikumar@gmail.com>
2024-01-13 10:48:48 +0530

    image updated

--- src/components/UserContextComponents.jsx
+++ src/components/UserContextComponents.jsx
@@ -14,7 +14,14 @@ function UserContextComponents({children}){
             "stock": 94,
             "brand": "Apple",
             "category": "smartphones",
-           
+            "thumbnail": "https://www.hindustantimes.com/ht-img/img/2023/10/01/1600x900/Apple-iPhone-15-plus--HT-Photo-Vishal-Mathur-_1696142455042.jpg",
+            "images": [
+                "https://i.dummyjson.com/data/products/1/1.jpg",
+                "https://i.dummyjson.com/data/products/1/2.jpg",
+                "https://i.dummyjson.com/data/products/1/3.jpg",
+                "https://i.dummyjson.com/data/products/1/4.jpg",
+                "https://i.dummyjson.com/data/products/1/thumbnail.jpg"
+            ]
         },
         {
             "id": 2,
@@ -26,7 +33,13 @@ function UserContextComponents({children}){
             "stock": 34,
             "brand": "Apple",
             "category": "smartphones",
-            
+            "thumbnail":"",
+            "images": [
+                "https://i.dummyjson.com/data/products/2/1.jpg",
+                "https://i.dummyjson.com/data/products/2/2.jpg",
+                "https://i.dummyjson.com/data/products/2/3.jpg",
+                "https://i.dummyjson.com/data/products/2/thumbnail.jpg"
+            ]
         },
         {
             "id": 3,
@@ -38,7 +51,10 @@ function UserContextComponents({children}){
             "stock": 36,
             "brand": "Samsung",
             "category": "smartphones",
-            
+            "thumbnail": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGNJNe0HIi2jfZPsaIdM-AcQhG7fogOggoWw&usqp=CAU",
+            "images": [
+                "https://i.dummyjson.com/data/products/3/1.jpg"
+            ]
         },
         {
             "id": 4,
@@ -50,7 +66,14 @@ function UserContextComponents({children}){
             "stock": 123,
             "brand": "OPPO",
             "category": "smartphones",
-            
+            "thumbnail": "https://www.seekpng.com/png/detail/34-347506_oppo-to-start-selling-its-smartphones-in-the.png",
+            "images": [
+                "https://i.dummyjson.com/data/products/4/1.jpg",
+                "https://i.dummyjson.com/data/products/4/2.jpg",
+                "https://i.dummyjson.com/data/products/4/3.jpg",
+                "https://i.dummyjson.com/data/products/4/4.jpg",
+                "https://i.dummyjson.com/data/products/4/thumbnail.jpg"
+            ]
         },
         {
             "id": 5,
@@ -61,7 +84,13 @@ function UserContextComponents({children}){
             "rating": 4.09,
             "stock": 32,
             "brand": "Huawei",
-           
+            "category": "smartphones",
+            "thumbnail": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1-D58PfT1NpqSZkPqgHkf92Z25RdEfztcxA&usqp=CAU",
+            "images": [
+                "https://i.dummyjson.com/data/products/5/1.jpg",
+                "https://i.dummyjson.com/data/products/5/2.jpg",
+                "https://i.dummyjson.com/data/products/5/3.jpg"
+            ]
         }
     ]
 );
